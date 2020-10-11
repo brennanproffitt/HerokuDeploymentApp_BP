d337f7f502e7723679549ca6546990bcab32cf3d
Brennan Proffitt <bmproffitt@gus.pittstate.edu>
2020-10-11 14:26:18 -0500

    Modified console.log info

--- controllers/birdsController.js
+++ controllers/birdsController.js
@@ -7,13 +7,15 @@ exports.get_birds_create = function(req,res) {
 }
 
 exports.post_birds_create = function(req,res) {
-        //process data/save to db
-        console.log(req.body.species);
-        console.log(req.body.nickname);
-        console.log(req.body.status);
-    
+        //format into JSON in order to save to database
+        let newBird = {
+            species: req.body.species,
+            nickname: req.body.nickname,
+            status: req.body.status
+        }
+
         //todo: save these to a database
-    
+        console.log(newBird);
         //what next? Get user to a confirmation page
         res.redirect('/birds/create/conf');
 }
